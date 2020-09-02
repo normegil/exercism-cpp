30805a2f552e663e3358168a629e07edd0312f8c
Olivier Ory <olivier.ory@uliege.be>
2020-09-02 14:14:39 +0000

    Exercism: Isogram (Solved)

--- cpp/isogram/isogram.cpp
+++ cpp/isogram/isogram.cpp
@@ -1,5 +1,30 @@
 #include "isogram.h"
 
-namespace isogram {
+#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <locale>
 
-}  // namespace isogram
+namespace isogram
+{
+    bool is_isogram(const std::string toCheck)
+    {
+        std::list<char> found = {};
+        for (const char to_find : toCheck)
+        {
+            if (' ' == to_find || '-' == to_find)
+            {
+                continue;
+            }
+
+            const char normalized = tolower(to_find, std::locale());
+            if (std::find(found.begin(), found.end(), normalized) != found.end())
+            {
+                return false;
+            }
+            found.push_back(normalized);
+        }
+        return true;
+    }
+} // namespace isogram
