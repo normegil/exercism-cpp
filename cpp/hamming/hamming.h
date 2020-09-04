2064ff06a01b76a57c90c8a8d622bb025d216fff
Olivier Ory <mail@normegil.be>
2020-09-04 05:18:01 +0000

    Hamming (Solved)

--- cpp/hamming/hamming.h
+++ cpp/hamming/hamming.h
@@ -1,8 +1,10 @@
 #if !defined(HAMMING_H)
 #define HAMMING_H
 
-namespace hamming {
+#include <string>
 
+namespace hamming {
+    int compute(std::string fst, std::string snd);
 }  // namespace hamming
 
 #endif // HAMMING_H
