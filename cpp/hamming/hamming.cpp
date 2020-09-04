5d02d3b6dc0a36420b8b6404dfbfc060816f7984
Olivier Ory <mail@normegil.be>
2020-09-04 05:36:39 +0000

    Hamming (SOlved)

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,24 +1,5 @@
 #include "hamming.h"
 
-#include <stdexcept>
+namespace hamming {
 
-namespace hamming
-{
-    int compute(std::string fst, std::string snd)
-    {
-
-        if (fst.length() != snd.length()) {
-            throw std::domain_error("Length doesn't match");
-        }
-
-        int count = 0;
-        for (decltype(fst.length()) i = 0; i < fst.length(); i++)
-        {
-            if (fst[i] != snd [i])
-            {
-                count+=1;
-            }
-        }
-        return count;
-    }
-} // namespace hamming
+}  // namespace hamming
--- cpp/hamming/hamming.h
+++ cpp/hamming/hamming.h
@@ -1,10 +1,8 @@
 #if !defined(HAMMING_H)
 #define HAMMING_H
 
-#include <string>
-
 namespace hamming {
-    int compute(std::string fst, std::string snd);
+
 }  // namespace hamming
 
 #endif // HAMMING_H
